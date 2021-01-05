e3ebc825323136b588bc2eb5dd8c7a09b179e34c
EvanDAB <carr916@gmail.com>
2021-01-05 13:10:41 -0800

    completed change password functionality, moved to reset password functionality, created forms for calculator-later commented out

--- paybackgas/account/forms.py
+++ paybackgas/account/forms.py
@@ -3,3 +3,11 @@ from django import forms
 class LoginForm(forms.Form):
     username=forms.CharField()
     password=forms.CharField(widget=forms.PasswordInput)
+
+class SimpleCalculatorForm(forms.Form):
+    mpg=forms.IntegerField(label='Miles Per Gallon')
+    gasprice=forms.IntegerField(label='Gas Price')
+    distance=forms.IntegerField(label='Distance')
+
+# class MPGCalculatorForm(forms.Form):
+    
--- paybackgas/account/views.py
+++ paybackgas/account/views.py
@@ -2,6 +2,20 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
+from .forms import SimpleCalculatorForm
+
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def dashboard(request):
+    mainform= SimpleCalculatorForm()
+    if mainform.is_valid():
+        mpg = mainform.cleaned_data['mpg']
+        distance = mainform.cleaned_data['distance']
+        gasprice = mainform.cleaned_data['gasprice']
+        print()
+    # return render(request, 'account/dashboard.html', {'form': mainform})
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
 
 def user_login(request):
     if request.method == 'POST':
