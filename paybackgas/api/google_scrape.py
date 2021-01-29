e246e49f6e6dcaccc5790c2cc80e5aab6ff0345a
EvanDAB <carr916@gmail.com>
2021-01-28 20:02:11 -0800

    made if statement single line if possible

--- paybackgas/api/google_scrape.py
+++ paybackgas/api/google_scrape.py
@@ -63,27 +63,4 @@ def determine_distance(*dest, data_type='json'):
         pass
 
 print(determine_distance('5112 Gorham Ct, Sacramento, CA', '674 23rd St, Oakland, CA', 'San Francisco, CA', 'Vallejo, CA'))    
-
-# def extract_lat_lng(address_or_postalcode, data_type='json'):
-#     endpoint= f"https://maps.googleapis.com/maps/api/geocode/{data_type}"
-#     params = {'address': address_or_postalcode, 'key': api_key}
-#     url_params = urlencode(params)
-#     url = f'{endpoint}?{url_params}'
-#     r = requests.get(url)
-#     print(url)
-#     if r.status_code not in range(200, 299):
-#         return {}
-#     latlng = {}
-#     try:
-#         latlng = r.json()['results'][0]['geometry']['location']
-#     except:
-#         pass
-#     return latlng.get('lat'), latlng.get('lng')
-#     # return r.json()
-
-# print(extract_lat_lng('1600 Ampitheatre Parkway, Mountain View, CA'))
-# to_parse = 'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Ampitheatre+Parkway%2C+Mountain+View%2C+CA&key=AIzaSyCNyQlXhkdhJQ4iczZl79qrzPf8EJXhzL8'
-# urlparse(to_parse)
-# query_string = urlparse(to_parse).query
-# print(query_string)
 print('Goodbye World')
