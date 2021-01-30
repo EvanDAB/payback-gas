2780a252a25b4010f66970b1a65569965db3bbed
EvanDAB <carr916@gmail.com>
2021-01-29 16:35:02 -0800

    successfully integrated static files

--- paybackgas/paybackgas/settings.py
+++ paybackgas/paybackgas/settings.py
@@ -124,7 +124,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
+# STATIC_ROOT = ''
+STATIC_URL = '/api/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # LOGIN_REDIRECT_URL = 'dashboard' #redirects user AFTER SUCCESSFUL login
 # LOGIN_URL = 'login'
